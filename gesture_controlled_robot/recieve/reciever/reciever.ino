97cfd8154213c7db06c83724c26ff92b8c14bfd9
Sudarshan S Harithas <48291759+sudarshan-s-harithas@users.noreply.github.com>
2019-06-29 21:34:32 +0530

    Add files via upload

---
+++ gesture_controlled_robot/recieve/reciever/reciever.ino
@@ -0,0 +1,95 @@
+/*
+ * The Reciever code for the Gesture Controlled Robot
+ * Data is transmitter through the RF Reciever (433Mhz)
+ * Author: Sudarshan S Harithas
+ */
+
+#include <VirtualWire.h>
+
+// Declaration ofpins for motor control
+int motrf=2;
+int motrb=4;
+int motlf=7;
+int motlb=8;
+int enable1=9;
+int enable2=10;
+
+
+void setup()
+{
+  
+pinMode(motrf,OUTPUT);
+pinMode(motlf,OUTPUT);
+pinMode(motlb,OUTPUT);
+pinMode(motrb,OUTPUT);
+digitalWrite(enable1,HIGH);
+digitalWrite(enable2,HIGH);
+
+
+Serial.begin(9600);
+vw_set_ptt_inverted(true); // Required for DR3100
+vw_set_rx_pin(6);
+vw_setup(4000);  // Bits per sec
+pinMode(13, OUTPUT);
+Serial.begin(9600);
+vw_rx_start();       // Start the receiver PLL running
+
+}
+    void loop()
+{
+  
+uint8_t buf[VW_MAX_MESSAGE_LEN];
+uint8_t buflen = VW_MAX_MESSAGE_LEN;
+
+    if (vw_get_message(buf, &buflen)) // Non-blocking
+    {
+      Serial.println("hello");
+      
+      if(buf[0]=='1'){
+
+  
+   digitalWrite(13,1);
+   Serial.println("f");
+     digitalWrite(motrf,HIGH);
+      digitalWrite(motrb,LOW);
+      digitalWrite(motlf,HIGH);
+      digitalWrite(motlb,LOW);
+      Serial.print("ok");
+      }  
+   if(buf[0]=='0'){
+  digitalWrite(13,0);
+  Serial.println("s");
+       digitalWrite(motrf,LOW);
+      digitalWrite(motrb,LOW);
+      digitalWrite(motlf,LOW);
+      digitalWrite(motlb,LOW); Serial.print("ok");
+    }
+     if(buf[0]=='2'){
+  digitalWrite(13,0);
+  Serial.println("b");
+    digitalWrite(motrf,LOW);
+      digitalWrite(motrb,HIGH);
+      digitalWrite(motlf,LOW);
+      digitalWrite(motlb,HIGH); Serial.print("ok");
+
+}
+  if(buf[0]=='3'){
+  digitalWrite(13,0);
+  Serial.println("l");
+      digitalWrite(motrf,HIGH);
+      digitalWrite(motrb,LOW);
+      digitalWrite(motlf,LOW);
+      digitalWrite(motlb,HIGH); Serial.print("ok");
+
+}
+  if(buf[0]=='4'){
+  digitalWrite(13,0);
+  Serial.println("r");
+   digitalWrite(motrf,LOW);
+      digitalWrite(motrb,HIGH);
+      digitalWrite(motlf,HIGH);
+      digitalWrite(motlb,LOW); Serial.print("ok");
+
+}
+}
+}
---
+++ gesture_controlled_robot/transmitter/transmitter.ino
@@ -0,0 +1,120 @@
+/*
+ * The Transmitter code for the Gesture Controlled Robot
+ * Data is transmitter through the RF transmitter (433Mhz)
+ * Author: Sudarshan S Harithas
+ */
+
+
+#include <VirtualWire.h>
+char *controller;  // The data to be transmitted
+int xPin=A3;    //X axis input from Accelerometer
+int yPin=A2;  //Y axis input from Accelerometer
+long x;         //Variabe for storing X coordinates
+long y;         //Variabe for storing Y coordinates
+
+
+void setup() {
+
+Serial.begin(9600);
+pinMode(xPin,INPUT);
+pinMode(yPin,INPUT);
+pinMode(13,OUTPUT);
+vw_set_ptt_inverted(true); 
+vw_set_tx_pin(12);
+vw_setup(4000); 
+
+}
+
+void loop() {
+ 
+x = analogRead(xPin); //Reads X coordinates
+y = analogRead(yPin); //Reads Y coordinates
+if(x<340)      // Change the value for adjusting sensitivity  
+      forward();
+else if(x>400) // Change the value for adjusting sensitivity
+      backward();
+else if(y>400) // Change the value for adjusting sensitivity
+      right();
+else if(y<340) // Change the value for adjusting sensitivity
+      left();
+else
+      stop_();
+}
+
+/*
+ * Function Name : stop
+ * Functionality : to transmit 0 to stop the robot
+ */
+
+void stop_()
+{
+  
+controller="0";
+vw_send((uint8_t *)controller, strlen(controller));
+vw_wait_tx(); // Wait until the whole message is gone
+digitalWrite(13,1);
+Serial.println("stop");
+
+}
+
+/*
+ * Function Name : forward
+ * Functionality : to transmit 1 to move the robot forward
+ */
+
+void forward()
+{
+  
+controller="1";
+vw_send((uint8_t *)controller, strlen(controller));
+vw_wait_tx(); // Wait until the whole message is gone
+digitalWrite(13,1);
+Serial.println("forward");
+}
+
+
+/*
+ * Function Name : backward
+ * Functionality : to transmit 2 to move the robot backward
+ */
+ 
+void backward()
+{
+controller="2";
+vw_send((uint8_t *)controller, strlen(controller));
+vw_wait_tx(); // Wait until the whole message is gone
+digitalWrite(13,1);
+Serial.println("backward");
+}
+
+/*
+ * Function Name : left
+ * Functionality : to transmit 3 to move the robot left
+ */
+
+void left()
+{
+controller="3";
+vw_send((uint8_t *)controller, strlen(controller));
+vw_wait_tx(); // Wait until the whole message is gone
+digitalWrite(13,1);
+Serial.println("left");
+}
+
+
+
+/*
+ * Function Name : right
+ * Functionality : to transmit 4 to move the robot right
+ */
+
+
+void right()
+{
+controller="4";
+vw_send((uint8_t *)controller, strlen(controller));
+vw_wait_tx(); // Wait until the whole message is gone
+digitalWrite(13,1);
+Serial.println("right");
+}
+
